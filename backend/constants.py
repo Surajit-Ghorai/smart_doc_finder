fc1aca363a1e911f90795d716f0239d35fd071ca
Surajit Ghorai <ghoraisurajit2021@gmail.com>
2024-02-26 19:03:55 +0530

    updated: ingestion pipeline

---
+++ backend/constants.py
@@ -0,0 +1,12 @@
+"""It stores all constants"""
+# persist_dir path to store embeddings locally
+PERSIST_DIR = r"C:\Users\promact\new_project_task\Intelligent_document_finder\persist_dir"
+
+# cache_dir path to store embedding_model locally
+CACHE_DIR = r"C:\Users\promact\new_project_task\Intelligent_document_finder\cache_dir"
+
+# google drive folder id
+FOLDER_ID = "1iJ78DGUr-Jc-Gx2odvNuNTiBQDhUnLhE" #"1hzk7y9TZ0lCRhaIdOOLkoJRblduoIQ3p"
+
+# local pipeline storage
+PIPELINE_DIR = r"C:\Users\promact\new_project_task\Intelligent_document_finder\local_pipeline_storage"
