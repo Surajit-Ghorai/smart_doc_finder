153885c8d7780544a669ba8ce1d9871140f3618c
Surajit Ghorai <ghoraisurajit2021@gmail.com>
2024-02-26 18:42:14 +0530

    updated: google drive integration

---
+++ backend/google_drive.py
@@ -0,0 +1,12 @@
+"""
+Google drive integration:
+It will fetch data from Google and will return all documents
+"""
+from llama_index.readers.google import GoogleDriveReader
+
+loader = GoogleDriveReader()
+
+def load_data(folder_id: str):
+    '''takes a folder id and returns all documents'''
+    docs = loader.load_data(folder_id=folder_id)
+    return docs
