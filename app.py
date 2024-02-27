dcc2b75b05712f8205a453ff9a8b09352c0eea20
Surajit Ghorai <ghoraisurajit2021@gmail.com>
2024-02-27 18:45:01 +0530

    On branch main Changes to be committed:     modified:   app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 """main app to run the application connecting both frontend and backend"""
+from frontend import my_streamlit_app
+from backend import main
+
+def app():
+    print("running...")
+
+if __name__ == "__main__":
+    app()
